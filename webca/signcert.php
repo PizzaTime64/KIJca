0c905ef4e289b3beef962be1366258e91b8fd837
Hadrian <hadrian@hadrianbs.web.id>
2015-05-06 01:16:26 +0700

    signcert.php -> sign the CSR

--- webca/signcert.php
+++ webca/signcert.php
@@ -2,6 +2,7 @@
 include('Net/SSH2.php');
 include('File/X509.php');
 include('Crypt/RSA.php');
+include('connection.php');
 
 // Load CA privaate key from file "ca.key"
 $CAPrivKey = new Crypt_RSA();
@@ -10,21 +11,16 @@ $privatekey = fread($privatekeyfile, 4096);
 $CAPrivKey->loadKey($privatekey);
 
 echo "the private key for the CA cert (can be discarded):\r\n\r\n";
-echo $privatekey;
+//echo $privatekey;
 echo "\r\n\r\n";
 
 
 //get CSR data from DB
-//$query = "SELECT .... FROM csr WHERE id = '$id';";
-//$hasil = mysql_query($query);
-//$row = mysql_fetch_array($hasil);
-
-
-
-// create private key / x.509 cert for stunnel / website
-$privKey = new Crypt_RSA();
-extract($privKey->createKey());
-$privKey->loadKey($privatekey);
+$query = "SELECT pubkey, name FROM client WHERE id =2;";
+$hasil = mysql_query($query);
+$row = mysql_fetch_array($hasil);
+$publickey = $row[0];
+$nama = $row[1];
 
 $pubKey = new Crypt_RSA();
 $pubKey->loadKey($publickey);
@@ -32,25 +28,32 @@ $pubKey->setPublicKey();
 
 $subject = new File_X509();
 $subject->setPublicKey($pubKey);
-$subject->setDNProp('TESTPHPSECLIB', 'DEMOGAN');
+$subject->setDNProp($nama, $nama);
+$subject->setDomain($nama);
 
 
 $issuer = new File_X509();
 $issuer->setPrivateKey($CAPrivKey);
 $issuer->setDNProp('KIJ C CA', 'Certificate authority');
-$issuer->setDomain('hadrianbs.web.id/kijca');
+$issuer->setDomain('hadrianbs.web.id');
 $issuer->setDN($issuer->getDN());
 
 
-
 $x509 = new File_X509();
+$x509->setStartDate('-1 month');
+$x509->setEndDate('+1 month');
+
+//sign CRS
 $result = $x509->sign($issuer, $subject);
 echo "Certificate";
 //echo $privKey->getPrivateKey();
 //echo "\r\n";
+echo $publickey;
 $certificate = $x509->saveX509($result, FILE_X509_FORMAT_PEM);
 $certificate = addslashes($certificate);
 echo $certificate;
 echo "\r\n";
+$query = "UPDATE client SET certificate='$certificate' WHERE id = 2;";
+$res = mysql_query($query);
 
 ?>
