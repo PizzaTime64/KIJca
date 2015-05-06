9674527a02f947f3b13b9589e933723c11eec52c
Hadrian <hadrian@hadrianbs.web.id>
2015-05-06 23:55:29 +0700

    update signcert.php

--- webca/signcert.php
+++ webca/signcert.php
@@ -2,6 +2,7 @@
 include('Net/SSH2.php');
 include('File/X509.php');
 include('Crypt/RSA.php');
+include('connection.php');
 
 // Load CA privaate key from file "ca.key"
 $CAPrivKey = new Crypt_RSA();
@@ -9,22 +10,32 @@ $privatekeyfile = fopen("ca.key", "r");
 $privatekey = fread($privatekeyfile, 4096);
 $CAPrivKey->loadKey($privatekey);
 
-echo "the private key for the CA cert (can be discarded):\r\n\r\n";
-echo $privatekey;
-echo "\r\n\r\n";
+//echo "the private key for the CA cert (can be discarded):\r\n\r\n";
+//echo $privatekey;
+//echo "\r\n\r\n";
 
 
 //get CSR data from DB
-//$query = "SELECT .... FROM csr WHERE id = '$id';";
-//$hasil = mysql_query($query);
-//$row = mysql_fetch_array($hasil);
+$query = "SELECT country, state, locality, org, orgUnit, name, email, id, pubKey FROM tblcertificate WHERE id = 7;";
+$hasil = mysql_query($query);
+if($hasil === FALSE) { 
+    die(mysql_error()); // TODO: better error handling
+}
 
+$row = mysql_fetch_array($hasil);
+$country = $row[0];
+$state = $row[1];
+$locality = $row[2];
+$org = $row[3];
+$orgUnit = $row[4];
+$name = $row[5];
+$email = $row[6];
+$serial = $row[7];
+$publickey = $row[8];
 
-
-// create private key / x.509 cert for stunnel / website
-$privKey = new Crypt_RSA();
-extract($privKey->createKey());
-$privKey->loadKey($privatekey);
+//echo $country;
+//echo $serial;
+//echo $publickey;
 
 $pubKey = new Crypt_RSA();
 $pubKey->loadKey($publickey);
@@ -32,7 +43,8 @@ $pubKey->setPublicKey();
 
 $subject = new File_X509();
 $subject->setPublicKey($pubKey);
-$subject->setDNProp('TESTPHPSECLIB', 'DEMOGAN');
+$subject->setDNProp($country, $org, $name, $state, $locality, $serial);
+$subject->setDomain('hadrianbs.web.id');
 
 
 $issuer = new File_X509();
@@ -44,7 +56,11 @@ $issuer->setDN($issuer->getDN());
 
 
 $x509 = new File_X509();
+$x509->setStartDate('-1 month');
+$x509->setEndDate('+1 year');
+$x509->setSerialNumber(chr($serial));
 $result = $x509->sign($issuer, $subject);
+
 echo "Certificate";
 //echo $privKey->getPrivateKey();
 //echo "\r\n";
@@ -52,5 +68,4 @@ $certificate = $x509->saveX509($result, FILE_X509_FORMAT_PEM);
 $certificate = addslashes($certificate);
 echo $certificate;
 echo "\r\n";
-
 ?>
